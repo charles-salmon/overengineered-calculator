7e945cafb08594a22941b189f9ebe5a4159ba7e7
Charles Salmon <charles.salmon@intranel.com>
2019-07-02 14:54:53 +1200

    Improve upon readability of `SlackRequestSignatureValidator`.

--- src/slack-request-signature-validator.ts
+++ src/slack-request-signature-validator.ts
@@ -5,6 +5,8 @@ import timingSafeCompare from "tsscmp";
 
 import { SecretProvider } from "./secret-provider";
 
+const REQUEST_VERSION: string = "v0";
+
 @injectable()
 class SlackRequestSignatureValidator {
   private secretProvider: SecretProvider;
@@ -19,20 +21,48 @@ class SlackRequestSignatureValidator {
   }
 
   public async isSignatureValid(): Promise<boolean> {
-    const { headers } = this.request;
-    const requestSignature = headers["x-slack-signature"] as string | undefined;
-    const requestTimestamp = headers["x-slack-request-timestamp"];
+    const [requestSignature, requestTimestamp] = this.getSlackHeaders();
     if (!requestSignature || !requestTimestamp) {
       return false;
     }
 
-    const fiveMinutesAgo = Math.floor(Date.now() / 1000) - 60 * 5;
-    if (+requestTimestamp < fiveMinutesAgo) {
+    if (!this.isRecentRequest(requestTimestamp)) {
       return false;
     }
 
-    const [version] = requestSignature.split("=");
-    const { rawBody } = this.request;
+    if (
+      !timingSafeCompare(
+        requestSignature,
+        await this.hashRawRequestBody(requestTimestamp, this.request.rawBody)
+      )
+    ) {
+      return false;
+    }
+
+    return true;
+  }
+
+  private getSlackHeaders(): [string | undefined, string | undefined] {
+    const { headers } = this.request;
+    const requestSignature = headers["x-slack-signature"] as string | undefined;
+    const requestTimestamp = headers["x-slack-request-timestamp"] as
+      | string
+      | undefined;
+
+    return [requestSignature, requestTimestamp];
+  }
+
+  private isRecentRequest(requestTimestamp: string): boolean {
+    const now = Math.floor(Date.now() / 1000);
+    const fiveMinutes = 5 * 60;
+
+    return now - +requestTimestamp < fiveMinutes;
+  }
+
+  private async hashRawRequestBody(
+    nonce: string,
+    rawBody: Buffer
+  ): Promise<string> {
     const slackSigningSecret = await this.secretProvider.decryptSecretFromFile({
       bucketName: process.env.STORAGE_BUCKET_NAME,
       cryptoKeyPath: process.env.CRYPTO_KEY_PATH,
@@ -40,13 +70,9 @@ class SlackRequestSignatureValidator {
     });
     const hmac = crypto
       .createHmac("sha256", slackSigningSecret)
-      .update(`${version}:${requestTimestamp}:${rawBody}`);
-    const computedSignature = `${version}=${hmac.digest("hex")}`;
-    if (!timingSafeCompare(requestSignature, computedSignature)) {
-      return false;
-    }
+      .update(`${REQUEST_VERSION}:${nonce}:${rawBody}`);
 
-    return true;
+    return `${REQUEST_VERSION}=${hmac.digest("hex")}`;
   }
 }
 
